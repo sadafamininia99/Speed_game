40e85303d18dcb1601908f904256221df190773c
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2021-10-10 14:57:43 +0330

    get variables
    add variable

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-const word = document.getElementById('word');
-const text = document.getElementById('text');
-const scoreEl = document.getElementById('score');
-const timeEl= document.getElementById('time');
-const endgameEl= document.getElementById('end-game');
-const settings = document.getElementById('settings');
-const settingsForm= document.getElementById('setting-form');
-const difficultySelect = document.getElementById('difficulty');
+    const word = document.getElementById('word');
+    const text = document.getElementById('text');
+    const scoreEl = document.getElementById('score');
+    const timeEl= document.getElementById('time');
+    const endgameEl= document.getElementById('end-game');
+    const settings = document.getElementById('settings');
+    const settingsForm= document.getElementById('setting-form');
+    const difficultySelect = document.getElementById('difficulty');
 
 
- 
-//LISt of word for games
-const words = [
-    'sigh',
-    'tense',
-    'airplane',
-    'ball',
-    'pies',
-    'juice',
-    'warlike',
-    'bad',
-    'north',
-    'dependent',
-    'steer',
-    'silver',
-    'highfalutin',
-    'superficial',
-    'quince',
-    'eight',
-    'feeble',
-    'admit',
-    'drag',
-    'loving'
-  ];
-  
+    
+    //LISt of word for games
+    const words = [
+        'sigh',
+        'tense',
+        'airplane',
+        'ball',
+        'pies',
+        'juice',
+        'warlike',
+        'bad',
+        'north',
+        'dependent',
+        'steer',
+        'silver',
+        'highfalutin',
+        'superficial',
+        'quince',
+        'eight',
+        'feeble',
+        'admit',
+        'drag',
+        'loving'
+    ];
+    
+    //Init word
+let randomWord;
+
+//init score
+let score =0;
+
+//init time
+let time=10;
