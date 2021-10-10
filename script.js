86e78ed0a823168a7053300b92e2ecf6c2811909
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2021-10-10 14:53:03 +0330

    Update script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl= document.getElementById('time');
+const endgameEl= document.getElementById('end-game');
+const settings = document.getElementById('settings');
+const settingsForm= document.getElementById('setting-form');
+const difficultySelect = document.getElementById('difficulty');
+
+
+ 
+//LISt of word for games
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+  ];
+  
