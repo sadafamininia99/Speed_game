110d7e382d7222a6a5eb01c92aa55619ec2d9cda
sadafamininia99 <aminicomputer96@gmail.com>
2021-10-17 19:16:57 +0330

    initial commit

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-    const word = document.getElementById('word');
-    const text = document.getElementById('text');
-    const scoreEl = document.getElementById('score');
-    const timeEl= document.getElementById('time');
-    const endgameEl= document.getElementById('end-game');
-    const settings = document.getElementById('settings');
-    const settingsForm= document.getElementById('setting-form');
-    const difficultySelect = document.getElementById('difficulty');
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
 
+// List of words for game
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving'
+];
 
-    
-    //LISt of word for games
-    const words = [
-        'sigh',
-        'tense',
-        'airplane',
-        'ball',
-        'pies',
-        'juice',
-        'warlike',
-        'bad',
-        'north',
-        'dependent',
-        'steer',
-        'silver',
-        'highfalutin',
-        'superficial',
-        'quince',
-        'eight',
-        'feeble',
-        'admit',
-        'drag',
-        'loving'
-    ];
-    
-    //Init word
+// Init word
 let randomWord;
 
-//init score
-let score =0;
+// Init score
+let score = 0;
 
-//init time
-let time=10;
+// Init time
+let time = 10;
+
+// Set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
