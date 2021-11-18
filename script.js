2ae75ed4abd255d81b25757fa37049d76a54a32f
Mohammad javad Masoudian <masoudian.dev@gmail.com>
2021-11-18 14:42:00 +0330

    Add lots of words

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
 
 // List of words for game
-const words = [
-  'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
-  'bad',
-  'north',
-  'dependent',
-  'steer',
-  'silver',
-  'highfalutin',
-  'superficial',
-  'quince',
-  'eight',
-  'feeble',
-  'admit',
-  'drag',
-  'loving'
-];
+// const words = [
+//   'sigh',
+//   'tense',
+//   'airplane',
+//   'ball',
+//   'pies',
+//   'juice',
+//   'warlike',
+//   'bad',
+//   'north',
+//   'dependent',
+//   'steer',
+//   'silver',
+//   'highfalutin',
+//   'superficial',
+//   'quince',
+//   'eight',
+//   'feeble',
+//   'admit',
+//   'drag',
+//   'loving'
+// ];
 
 // Init word
 let randomWord;
